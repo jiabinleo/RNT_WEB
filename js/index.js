b7feaee32df8bc3cd5bac1b9827c1b682109bdc5
jiabinleo <jiabinleo@126.com>
2018-09-25 16:26:10 +0800

    服务中心静态页面完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(function () {
-
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 3000,
+        loop: true,
+        speed: 1000,
+        // slidesPerView: 'auto',
+        pagination: '.swiper-pagination',
+        autoplayDisableOnInteraction: false,
+        centeredSlides: true
+    })
     $("#center").click(function () {
         $(".masked").addClass(" maskeds")
         $(".personalCente").addClass(" peractive")
