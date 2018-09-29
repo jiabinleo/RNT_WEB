c5d9ad5d9568cf6249f5a721f81d8fffd15b4392
jiabinleo <jiabinleo@126.com>
2018-09-29 18:19:10 +0800

    新增三级联动插件，待完成

---
+++ view/homePage/supplyAndDemandInner/js/supplyAndDemandInner.js
@@ -0,0 +1,338 @@
+$(function () {
+    var supDem = {
+        init: function () {
+            supDem.listen()
+        },
+        listen: function () {
+            var contentData = [{
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }, {
+                user: "赣南脐橙专业合作社",
+                userImgUrl: "img/userLogo.png",
+                iden: "江西赣州赣南县",
+                title: "出售赣南脐橙",
+                time: "1小时前",
+                text: "本合作社种植赣南脐橙50亩地，产量约20万斤，现出售，有需要的请联系电话18889878888。",
+                read: "888",
+                heart: "666",
+                img: [
+                    "img/new.png", "img/new.png", "img/new.png", "img/new.png"
+                ]
+            }]
+
+            var contentHTML = "";
+            for (let i = 0; i < contentData.length; i++) {
+                var imgList = ""
+                var imgLength = contentData[i].img.length > 3 ? 3 : contentData[i].img.length
+                for (let j = 0; j < imgLength; j++) {
+                    imgList += `<li><img src="${contentData[i].img[j]}" alt=""></li>`
+                }
+                contentHTML += `<div class="supplyAndDemand-text">
+                <div class="supplyAndDemand-text-title">
+                    <div class="left">
+                        <img src="${contentData[i].userImgUrl}" alt="">
+                    </div>
+                    <div class="center">
+                        <p>${contentData[i].user}</p>
+                        <p><span>${contentData[i].iden}</span></p>
+                    </div>
+                    <div class="right">
+                        <p>${contentData[i].time}</p>
+                    </div>
+                </div>
+                <div class="supplyAndDemand-text-content">
+                    <p>${contentData[i].title}</p>
+                    <p>${contentData[i].text}</p>
+                </div>
+                <div class="supplyAndDemand-text-img">
+                    <ul>
+                        ${imgList}
+                    </ul>
+                </div>
+                <div class="count">
+                    <p>
+                        <span><img src="img/read.png" alt="">${contentData[i].read}人阅读</span>
+                        <span><img src="img/heart.png" alt="">${contentData[i].heart}人阅读</span>
+                    </p>
+                </div>
+            </div>
+        </div>`
+            }
+            $("#contentHTML").html(contentHTML)
+        }
+    }
+    supDem.init()
+
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 3000,
+        loop: true,
+        speed: 1000,
+        // slidesPerView: 'auto',
+        pagination: '.swiper-pagination',
+        autoplayDisableOnInteraction: false,
+        centeredSlides: true
+    })
+
+
+    var nameEl = document.getElementById('picker');
+
+    var first = []; /* 省，直辖市 */
+    var second = []; /* 市 */
+    var third = []; /* 镇 */
+
+    var selectedIndex = [0, 0, 0]; /* 默认选中的地区 */
+
+    var checked = [0, 0, 0]; /* 已选选项 */
+
+    function creatList(obj, list) {
+        obj.forEach(function (item, index, arr) {
+            var temp = new Object();
+            temp.text = item.name;
+            temp.value = index;
+            list.push(temp);
+        })
+    }
+
+    creatList(city, first);
+
+    if (city[selectedIndex[0]].hasOwnProperty('sub')) {
+        creatList(city[selectedIndex[0]].sub, second);
+    } else {
+        second = [{
+            text: '',
+            value: 0
+        }];
+    }
+
+    if (city[selectedIndex[0]].sub[selectedIndex[1]].hasOwnProperty('sub')) {
+        creatList(city[selectedIndex[0]].sub[selectedIndex[1]].sub, third);
+    } else {
+        third = [{
+            text: '',
+            value: 0
+        }];
+    }
+
+    var picker = new Picker({
+        data: [first, second, third],
+        selectedIndex: selectedIndex,
+        title: ''
+    });
+
+    picker.on('picker.select', function (selectedVal, selectedIndex) {
+        var text1 = first[selectedIndex[0]].text;
+        var text2 = second[selectedIndex[1]].text;
+        var text3 = third[selectedIndex[2]] ? third[selectedIndex[2]].text : '';
+        nameEl.value = text1 + ' ' + text2 + ' ' + text3;
+    });
+
+    picker.on('picker.change', function (index, selectedIndex) {
+        if (index === 0) {
+            firstChange();
+        } else if (index === 1) {
+            secondChange();
+        }
+
+        function firstChange() {
+            second = [];
+            third = [];
+            checked[0] = selectedIndex;
+            var firstCity = city[selectedIndex];
+            if (firstCity.hasOwnProperty('sub')) {
+                creatList(firstCity.sub, second);
+
+                var secondCity = city[selectedIndex].sub[0]
+                if (secondCity.hasOwnProperty('sub')) {
+                    creatList(secondCity.sub, third);
+                } else {
+                    third = [{
+                        text: '',
+                        value: 0
+                    }];
+                    checked[2] = 0;
+                }
+            } else {
+                second = [{
+                    text: '',
+                    value: 0
+                }];
+                third = [{
+                    text: '',
+                    value: 0
+                }];
+                checked[1] = 0;
+                checked[2] = 0;
+            }
+
+            picker.refillColumn(1, second);
+            picker.refillColumn(2, third);
+            picker.scrollColumn(1, 0)
+            picker.scrollColumn(2, 0)
+        }
+
+        function secondChange() {
+            third = [];
+            checked[1] = selectedIndex;
+            var first_index = checked[0];
+            if (city[first_index].sub[selectedIndex].hasOwnProperty('sub')) {
+                var secondCity = city[first_index].sub[selectedIndex];
+                creatList(secondCity.sub, third);
+                picker.refillColumn(2, third);
+                picker.scrollColumn(2, 0)
+            } else {
+                third = [{
+                    text: '',
+                    value: 0
+                }];
+                checked[2] = 0;
+                picker.refillColumn(2, third);
+                picker.scrollColumn(2, 0)
+            }
+        }
+
+    });
+
+    picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
+        console.log(selectedVal);
+        console.log(selectedIndex);
+    });
+
+    nameEl.addEventListener('click', function () {
+        picker.show();
+    });
+
+
+
+
+
+})
