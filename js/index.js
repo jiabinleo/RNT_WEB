61bcd0d5a30ce77de54d907caf4ad47304668f3a
jiabinleo <jiabinleo@126.com>
2018-10-08 09:36:10 +0800

    主页banner部分由js生成

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
 $(function () {
+    var indexPage = {
+        init: function () {
+            indexPage.listen()
+        },
+        listen: function () {
+            var bannerData = ["img/banner01.png", "img/banner01.png", "img/banner01.png", "img/banner01.png", "img/banner01.png", "img/banner01.png"]
+            var bannerHTML = "";
+            for (let i = 0; i < bannerData.length; i++) {
+                bannerHTML +=
+                    `<div class="swiper-slide">
+                        <img src="${bannerData[i]}" alt="banner01">
+                    </div>`
+            }
+            $("#banner").html(bannerHTML)
+            $("#center").click(function () {
+                $(".masked").addClass(" maskeds")
+                $(".personalCente").addClass(" peractive")
+            })
+            $(".masked").click(function () {
+                $(".masked").removeClass(" maskeds")
+                $(".personalCente").removeClass(" peractive")
+            })
+            $(".personalCente_content > ul > li").click(function () {
+                $(this).addClass("active").siblings().removeClass("active")
+            })
+        }
+    }
+    indexPage.init()
     var mySwiper = new Swiper('.swiper-container', {
         autoplay: 3000,
         loop: true,
@@ -8,15 +36,5 @@ $(function () {
         autoplayDisableOnInteraction: false,
         centeredSlides: true
     })
-    $("#center").click(function () {
-        $(".masked").addClass(" maskeds")
-        $(".personalCente").addClass(" peractive")
-    })
-    $(".masked").click(function () {
-        $(".masked").removeClass(" maskeds")
-        $(".personalCente").removeClass(" peractive")
-    })
-    $(".personalCente_content > ul > li").click(function () {
-        $(this).addClass("active").siblings().removeClass("active")
-    })
+
 })
