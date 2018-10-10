d3be4d2703762cebcc37b32a83ece8b7eb5ecdfb
jiabinleo <jiabinleo@126.com>
2018-10-10 18:07:51 +0800

    我要贷款页面和我的账单页面

--- view/homePage/debit/js/debit.js
+++ view/homePage/debit/js/debit.js
@@ -1,42 +1,174 @@
-var t;
-var e = 89432.61/100000*100
-$(document).ready(function () {
-    i = 0;
-    t = setInterval(function () {
-        addNum(e, 150)
-    }, 20);
-});
+var debit = {
+    init: function () {
+        debit.listen()
+    },
+    listen: function () {
 
-function addNum(percent, width) {
-    if (i < percent) {
-        i++;
-        $('canvas.debit_process').text(i + "%");
-        drawProcess(width);
-    } else {
-        clearInterval(t);
+
+        var bannerData = ["img/banner1.png", "img/banner2.png", "img/banner3.png", "img/banner1.png", "img/banner2.png", "img/banner3.png"];
+        var bannerImgHTML = ""
+        for (let i = 0; i < bannerData.length; i++) {
+            bannerImgHTML +=
+                `<div class="swiper-slide">
+                    <a href="#">
+                        <img src="${bannerData[i]}" alt="banner"></a>
+                </div>`
+        }
+        $("#bannerImgHTML").html(bannerImgHTML)
+
+        var menuData = [{
+            link: "#",
+            imgUrl: "img/menu1.png",
+            title: "种植贷"
+        }, {
+            link: "#",
+            imgUrl: "img/menu2.png",
+            title: "农资贷"
+        }, {
+            link: "#",
+            imgUrl: "img/menu3.png",
+            title: "农贸贷"
+        }, {
+            link: "#",
+            imgUrl: "img/menu4.png",
+            title: "个人贷"
+        }]
+        var menuHTML = ""
+        for (let i = 0; i < menuData.length; i++) {
+            menuHTML +=
+                `<li>
+                    <a href="${menuData[i].link}">
+                        <div>
+                            <div class="img">
+                                <img src="${menuData[i].imgUrl}" alt="">
+                            </div>
+                            <p>${menuData[i].title}</p>
+                        </div>
+                    </a>
+                </li>`
+        }
+        $("#menuHTML").html(menuHTML)
+
+        var data = [{
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "柑橘头号杀手“黄龙病”柑橘头号杀手“黄龙病”柑橘头号杀手“黄龙病”"
+            },
+            {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            },
+            {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "柑橘头号杀手“黄龙病”柑橘头号杀手“黄龙病”柑橘头号杀手“黄龙病”"
+            },
+            {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new2.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new3.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            }, {
+                link: "#",
+                img: "img/new1.png",
+                text: "两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病两套“三板斧”对付柑橘黄龙病"
+            },
+
+        ]
+        var caseOfLoan = ""
+        for (let i = 0; i < data.length; i++) {
+            caseOfLoan +=
+                `<li>
+            <a href="${data[i].link}">
+                <div>
+                    <img src="${data[i].img}" alt="">
+                </div>
+                <p>${data[i].text}</p>
+            </a>
+        </li>`
+        }
+        $("#caseOfLoan").html(caseOfLoan)
+
+        debit.plugin()
+    },
+    plugin: function () {
+        var mySwiper = new Swiper(".swiper-container", {
+            autoplay: 3000,
+            loop: true,
+            speed: 1000,
+            // slidesPerView: 'auto',
+            pagination: ".swiper-pagination",
+            autoplayDisableOnInteraction: false,
+            centeredSlides: true
+        });
     }
 }
-
-function drawProcess(width) {
-    $('canvas.debit_process').each(function () {
-        var text = $(this).text();
-        var process = text.substring(0, text.length - 1);
-        var canvas = this;
-        var context = canvas.getContext('2d');
-        context.clearRect(0, 0, width, width);
-        //环前景色渐变色  
-        context.beginPath();
-        context.moveTo(width / 2, width / 2);
-        context.arc(width / 2, width / 2, width / 2, Math.PI * 1.5, Math.PI * 1.5 * process / 100, false);
-        context.closePath();
-        var lGrd = context.createLinearGradient(0, 0, 300, 300);
-        lGrd.addColorStop(0, '#ffffff');
-        context.fillStyle = lGrd;
-        context.fill();
-        //设置内圆透明
-        context.beginPath();
-        context.arc(width / 2, width / 2, width / 2 - 10, 0, Math.PI * 2, true);
-        context.fillStyle = 'rgba(56,187,240,1)';
-        context.fill();
-    });
-}
+debit.init()
---
+++ view/homePage/myBills/js/myBills.js
@@ -0,0 +1,214 @@
+var myBills = {
+    init: function () {
+        myBills.listen()
+    },
+    listen: function () {
+        var data = [{
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96000",
+                status: 1,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96001",
+                status: 2,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            },
+            {
+                num: "96002",
+                status: 3,
+                text: "期数24 剩余0期",
+                time: "2015年1月24日"
+            }
+        ]
+        var myBillsHTML = "";
+        for (let i = 0; i < data.length; i++) {
+            myBillsHTML +=
+                `<li>
+            <div class="billsTop">
+                <span>
+                    ￥${data[i].num}
+                </span>
+                <span class="status${data[i].status}">
+                    ${myBills.status(data[i].status)}
+                </span>
+            </div>
+            <div class="billsBottom">
+                <span>
+                    期数24 剩余12期
+                </span>
+                <span>
+                    <img src="img/time.png" alt="time">
+                    2017年12月12日
+                </span>
+            </div>
+        </li>`
+
+        }
+        $("#myBillsHTML").html(myBillsHTML)
+    },
+    status: function (status) {
+        ss = "";
+        switch (status) {
+
+            case 1:
+                ss = "已还款"
+                break;
+            case 2:
+                ss = "未还款"
+                break;
+            case 3:
+                ss = "已还清"
+                break;
+            default:
+                break;
+        }
+        return ss
+    }
+}
+myBills.init()
