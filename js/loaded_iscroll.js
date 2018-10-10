4df94e640551481206a302bedc14f0953dac5fd1
jiabinleo <jiabinleo@126.com>
2018-10-10 10:06:30 +0800

    解决在isScrool下无法点击的问题

--- js/loaded_iscroll.js
+++ js/loaded_iscroll.js
@@ -1,5 +1,16 @@
 //isScrool
 var myScroll;
+
+function iScrollClick() {
+    if (/iPhone|iPad|iPod|Macintosh/i.test(navigator.userAgent)) return false;
+    if (/Chrome/i.test(navigator.userAgent)) return (/Android/i.test(navigator.userAgent));
+    if (/Silk/i.test(navigator.userAgent)) return false;
+    if (/Android/i.test(navigator.userAgent)) {
+        var s = navigator.userAgent.substr(navigator.userAgent.indexOf('Android') + 8, 3);
+        return parseFloat(s[0] + s[3]) < 44 ? false : true
+    }
+}
+
 function isPassive() {
     var supportsPassiveOption = false;
     try {
@@ -8,17 +19,23 @@ function isPassive() {
                 supportsPassiveOption = true;
             }
         }));
-    } catch(e) {}
+    } catch (e) {}
     return supportsPassiveOption;
 }
+
 function loaded() {
     myScroll = new IScroll('#wrapper', {
         scrollbars: true,
         mouseWheel: true,
-        click:true,
         interactiveScrollbars: true,
         shrinkScrollbars: 'scale',
-        fadeScrollbars: true
+        fadeScrollbars: true,
+        scrollY: true,
+        probeType: 2,
+        bindToWrapper: true,
+        click: iScrollClick(), //调用判断函数
+        taps: true,
+        preventDefault: false
     });
 }
 document.addEventListener('touchmove', function (e) {
@@ -28,6 +45,6 @@ document.addEventListener('touchmove', function (e) {
     passive: false
 } : false);
 
-document.body.onload=function(){
+document.body.onload = function () {
     loaded()
 }
