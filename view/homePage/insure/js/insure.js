3e7c2733e03e75b524aa06471987cb6c0539cb07
jiabinleo <jiabinleo@126.com>
2018-10-10 15:36:16 +0800

    投保页面增加交互

--- view/homePage/insure/js/insure.js
+++ view/homePage/insure/js/insure.js
@@ -1,49 +1,79 @@
-$(function(){
+$(function () {
     var insure = {
-        init : function(){
-            $("#playFooter").on("click",function(){
-                $("#insure_wrap").css("display","block")
-                $("#insure_inner").css("display","block")
+        init: function () {
+            if (localStorage.getItem("msg") == null || localStorage.getItem("msg") == "null") {
+                msg = [{
+                    number: "请输入投保亩数",
+                    name: "请输入你的姓名",
+                    idType: "请选择你的证件类型",
+                    id: "请输入你的证件号码",
+                    pho: "请输入你的手机号",
+                    ema: "请输入你的电子邮箱",
+                    msg: "请输入承保作物信息",
+                    ben: "请输入受益人名称",
+                }]
+                localStorage.setItem("msg", JSON.stringify(msg))
+            }
+            var msg = JSON.parse(localStorage.getItem("msg"))[0]
+            $("#number").html((parseFloat(msg.number) ? parseFloat(msg.number) : 0) + "亩")
+            $("#name").html(msg.name)
+            $("#idType").html(msg.idType)
+            $("#id").html(msg.id)
+            $("#pho").html(msg.pho)
+            $("#ema").html(msg.ema)
+            $("#msg").html(msg.msg)
+            $("#ben").html(msg.ben)
+            if (parseFloat(msg.number)) {
+                $("#priceSum").html(parseFloat($("#priceDan").html()) * parseFloat($("#number").html()))
+            } else {
+                $("#priceSum").html(0)
+            }
+
+            insure.listen()
+        },
+        listen: function () {
+            $("#playFooter").on("click", function () {
+                $("#insure_wrap").css("display", "block")
+                $("#insure_inner").css("display", "block")
             })
-            $("#insureClose").on("click",function(){
-                $("#insure_wrap").css("display","none")
-                $("#insure_inner").css("display","none")
+            $("#insureClose").on("click", function () {
+                $("#insure_wrap").css("display", "none")
+                $("#insure_inner").css("display", "none")
             })
-            $(".insure_play_bottom").on("click",function(){
-                $("#insure_inner").css("display","none")
-                $("#insure_inner2").css("display","block")
+            $(".insure_play_bottom").on("click", function () {
+                $("#insure_inner").css("display", "none")
+                $("#insure_inner2").css("display", "block")
             })
-            $("#insureClose2").on("click",function(){
-                $("#insure_wrap").css("display","block")
-                $("#insure_inner").css("display","block")
-                $("#insure_inner2").css("display","none")
-                for(var i=0;i<6;i++){
+            $("#insureClose2").on("click", function () {
+                $("#insure_wrap").css("display", "block")
+                $("#insure_inner").css("display", "block")
+                $("#insure_inner2").css("display", "none")
+                for (var i = 0; i < 6; i++) {
                     $(".mima input").eq(i).val("")
                 }
             })
-            
+
             var pwdarr = [];
-            var pwdarrs = ["1","2","3","4","5","6"]
-            $(".mima").on("click",function(){
+            var pwdarrs = ["1", "2", "3", "4", "5", "6"]
+            $(".mima").on("click", function () {
                 $(".mima input").eq(0).focus()
                 pwdarr = [];
             })
-            var pwss =true;
-			$(".mima > input").on("input", function() {
+            var pwss = true;
+            $(".mima > input").on("input", function () {
                 $(this).val($(this).val().trim().substr(0, 1))
                 pwdarr.push($(this).val().trim().substr(0, 1))
-                if($(this).next().length){
+                if ($(this).next().length) {
                     $(this).next().focus()
-                }else{
-                    if(pwdarr.length = 6){
+                } else {
+                    if (pwdarr.length = 6) {
                         setTimeout(() => {
-                            window.location.href="/project/view/playsuc/playsuc.html";
+                            window.location.href = "/project/view/playsuc/playsuc.html";
                         }, 1000);
                     }
                 }
-                
+
             });
-            
         }
     }
     insure.init()
---
+++ view/homePage/insure/js/insureInput.js
@@ -0,0 +1,67 @@
+$(function () {
+    var insureInput = {
+
+        init: function () {
+            this.listen()
+        },
+        listen: function () {
+            var urlIds = insureInput.getRequest().ids
+            var msg = JSON.parse(localStorage.getItem("msg"))[0]
+            console.log(msg)
+            if (urlIds === "number") {
+                $("#title").html("请输入投保亩数")
+                $("#text").attr("placeholder", "请输入投保亩数")
+                $("#text").attr("value", msg.number)
+            } else if (urlIds === "name") {
+                $("#title").html("请输入你的姓名")
+                $("#text").attr("placeholder", "请输入你的姓名")
+                $("#text").attr("value", msg.name)
+            } else if (urlIds === "idType") {
+                $("#title").html("请选择你的证件类型")
+                $("#text").attr("placeholder", "请选择你的证件类型")
+                $("#text").attr("value", msg.idType)
+            } else if (urlIds === "id") {
+                $("#title").html("请输入你的证件号码")
+                $("#text").attr("placeholder", "请输入你的证件号码")
+                $("#text").attr("value", msg.id)
+            } else if (urlIds === "pho") {
+                $("#title").html("请输入你的手机号")
+                $("#text").attr("placeholder", "请输入你的手机号")
+                $("#text").attr("value", msg.pho)
+            } else if (urlIds === "ema") {
+                $("#title").html("请输入你的电子邮箱")
+                $("#text").attr("placeholder", "请输入你的电子邮箱")
+                $("#text").attr("value", msg.ema)
+            } else if (urlIds === "msg") {
+                $("#title").html("请输入承保作物信息")
+                $("#text").attr("placeholder", "请输入承保作物信息")
+                $("#text").attr("value", msg.msg)
+            } else if (urlIds === "ben") {
+                $("#title").html("请输入受益人名称")
+                $("#text").attr("placeholder", "请输入受益人名称")
+                $("#text").attr("value", msg.ben)
+            }
+            $(document).on("click", "#sweep", function () {
+                msg[urlIds] = $("#text").val()
+                if ($("#text").val() == "") {
+                    msg[urlIds] = $("#text").attr("placeholder")
+                }
+                localStorage.setItem("msg", JSON.stringify([msg]))
+            })
+
+        },
+        getRequest: function () {
+            var url = window.location.search; //获取url中"?"符后的字串
+            var theRequest = new Object();
+            if (url.indexOf("?") != -1) {
+                var str = url.substr(1);
+                strS = str.split("&");
+                for (var i = 0; i < strS.length; i++) {
+                    theRequest[strS[i].split("=")[0]] = decodeURI(strS[i].split("=")[1]);
+                }
+            }
+            return theRequest;
+        }
+    }
+    insureInput.init()
+})
