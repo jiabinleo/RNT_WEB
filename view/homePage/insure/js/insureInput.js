499a766b2b28a36f5af6aa8ef557b43f5a5fc51a
jiabinleo <jiabinleo@126.com>
2018-10-17 09:25:07 +0800

    表单未输入时保持原来数据

--- view/homePage/insure/js/insureInput.js
+++ view/homePage/insure/js/insureInput.js
@@ -11,42 +11,47 @@ $(function () {
             if (urlIds === "number") {
                 $("#title").html("请输入投保亩数")
                 $("#text").attr("placeholder", "请输入投保亩数")
-                $("#text").attr("value", msg.number)
+                // if(){
+                // $("#text").attr("value", msg.number)
+                // }
+
             } else if (urlIds === "name") {
                 $("#title").html("请输入你的姓名")
                 $("#text").attr("placeholder", "请输入你的姓名")
-                $("#text").attr("value", msg.name)
+                // $("#text").attr("value", msg.name)
             } else if (urlIds === "idType") {
                 $("#title").html("请选择你的证件类型")
                 $("#text").attr("placeholder", "请选择你的证件类型")
-                $("#text").attr("value", msg.idType)
+                // $("#text").attr("value", msg.idType)
             } else if (urlIds === "id") {
                 $("#title").html("请输入你的证件号码")
                 $("#text").attr("placeholder", "请输入你的证件号码")
-                $("#text").attr("value", msg.id)
+                // $("#text").attr("value", msg.id)
             } else if (urlIds === "pho") {
                 $("#title").html("请输入你的手机号")
                 $("#text").attr("placeholder", "请输入你的手机号")
-                $("#text").attr("value", msg.pho)
+                // $("#text").attr("value", msg.pho)
             } else if (urlIds === "ema") {
                 $("#title").html("请输入你的电子邮箱")
                 $("#text").attr("placeholder", "请输入你的电子邮箱")
-                $("#text").attr("value", msg.ema)
+                // $("#text").attr("value", msg.ema)
             } else if (urlIds === "msg") {
                 $("#title").html("请输入承保作物信息")
                 $("#text").attr("placeholder", "请输入承保作物信息")
-                $("#text").attr("value", msg.msg)
+                // $("#text").attr("value", msg.msg)
             } else if (urlIds === "ben") {
                 $("#title").html("请输入受益人名称")
                 $("#text").attr("placeholder", "请输入受益人名称")
-                $("#text").attr("value", msg.ben)
+                // $("#text").attr("value", msg.ben)
             }
             $(document).on("click", "#sweep", function () {
                 msg[urlIds] = $("#text").val()
-                if ($("#text").val() == "") {
+                if ($.trim($("#text").val()) == "") {
                     msg[urlIds] = $("#text").attr("placeholder")
+                } else {
+                    localStorage.setItem("msg", JSON.stringify([msg]))
                 }
-                localStorage.setItem("msg", JSON.stringify([msg]))
+
                 window.open("insure.html", "_self");
             })
             // $(document).on("click", "#historyBack", function () {
