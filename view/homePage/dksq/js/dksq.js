93e062b1a191286a008383a1ac00d2d182b38c74
jiabinleo <jiabinleo@126.com>
2018-10-18 18:18:45 +0800

    新增所在地区选择

--- view/homePage/dksq/js/dksq.js
+++ view/homePage/dksq/js/dksq.js
@@ -1,40 +1,140 @@
-var onlineService = {
-    init: function () {
-        onlineService.listen()
-    },
-    listen: function () {
-        var content_listHTML = "";
-        for (let i = 0; i < data.length; i++) {
-            var lisHTML = ""
-            for (let j = 0; j < data[i].list.length; j++) {
-                lisHTML +=
-                    ` <li>(${j})${data[i].list[j].text}</li>`
+$(function () {
+    var locs = false;
+    var dksq = {
+        init: function () {
+            if (localStorage.getItem("msg") == null || localStorage.getItem("msg") == "null") {
+                var msg = [{
+                    mec: "请选择机构类型",
+                    org: "请输入机构名称或借款人姓名",
+                    loc: "请输入地址",
+                    lan: "请输入土地面积",
+                    man: "1",
+                    mar: "1",
+                    loa: "1", //0没有 1有
+                    col: "请输入",
+                    con: "请输入",
+                    num: "请输入",
+                    amo: "请输入",
+                    use: "请输入",
+                    sta: "请选择",
+                    end: "请选择",
+                }]
+                localStorage.setItem("msg", JSON.stringify(msg))
             }
-            content_listHTML +=
-                `<li class="list">
-                    <div class="title">
-                        <p>${i+1}.${data[i].title}</p>
-                        <span class="img"><img src="img/right.png" alt=""></span>
-                    </div>
-                    <ul class="content_list_text">
-                    ${lisHTML}
-                    </ul>
-                </li>`
-        }
+            var msg = JSON.parse(localStorage.getItem("msg"))[0]
+            console.log(msg)
+            $("#mec").html(msg.mec)
+            $("#org").html(msg.org)
+            $("#loc").html(msg.loc)
+            $("#lan").html(msg.lan)
+            $("#col").html(msg.col)
+            $("#con").html(msg.con)
+            $("#num").html(msg.num)
+            $("#amo").html(msg.amo)
+            $("#use").html(msg.use)
+            $("#sta").html(msg.sta)
+            $("#end").html(msg.end)
+            $("#man .status").eq(msg.man).find("img").attr("src", "img/yes.png")
+            $("#mar .status").eq(msg.mar).find("img").attr("src", "img/yes.png")
+            $("#loa .status").eq(msg.loa).find("img").attr("src", "img/yes.png")
+            dksq.listen()
+        },
+        listen: function () {
+            $(document).on("touchend", ".radius span", function () {
+                $(this).find("img").attr("src", "img/yes.png").parent().siblings().find("img").attr("src", "img/no.png")
+                var msg = JSON.parse(localStorage.getItem("msg"))[0]
+                msg[$(this).parent().attr("id")] = $(this).attr("data-id")
+                localStorage.setItem("msg", JSON.stringify([msg]))
+            })
 
-        $("#content_list").html(content_listHTML)
-        $(document).on("touchend", "#content_list .title", function () {
-            if ($(this).parent().hasClass("active")) {
-                $(this).parent().removeClass("active")
-                $(this).find("img").attr("src", "img/right.png")
-            } else {
-                $(this).parent().addClass("active")
-                $(this).find("img").attr("src", "img/bottom.png")
-            }
-            if (myScroll) {
-                myScroll.refresh();
-            }
-        })
+            $("#dd").on("click", function () {
+                locs = true;
+                console.log(loc)
+                $("#loc").html(loc)
+            })
+
+
+            $("#playFooter").on("click", function () {
+                $("#dksq_wrap").css("display", "block")
+                $("#dksq_inner").css("display", "block")
+            })
+            $("#dksqClose").on("click", function () {
+                $("#dksq_wrap").css("display", "none")
+                $("#dksq_inner").css("display", "none")
+            })
+            $(".dksq_play_bottom").on("click", function () {
+                $("#dksq_inner").css("display", "none")
+                $("#dksq_inner2").css("display", "block")
+            })
+            $("#dksqClose2").on("click", function () {
+                $("#dksq_wrap").css("display", "block")
+                $("#dksq_inner").css("display", "block")
+                $("#dksq_inner2").css("display", "none")
+                for (var i = 0; i < 6; i++) {
+                    $(".mima input").eq(i).val("")
+                }
+            })
+
+            var pwdarr = [];
+            var pwdarrs = ["1", "2", "3", "4", "5", "6"]
+            $(".mima").on("click", function () {
+                $(".mima input").eq(0).focus()
+                pwdarr = [];
+            })
+            var pwss = true;
+            $(".mima > input").on("input", function () {
+                $(this).val($(this).val().trim().substr(0, 1))
+                pwdarr.push($(this).val().trim().substr(0, 1))
+                if ($(this).next().length) {
+                    $(this).next().focus()
+                } else {
+                    if (pwdarr.length = 6) {
+                        setTimeout(() => {
+                            window.location.href = "/project/view/playsuc/playsuc.html";
+                        }, 1000);
+                    }
+                }
+
+            });
+            $(document).on("click", "#sweep", function () {
+                window.location.href = "../maize/maize.html";
+            })
+        },
+
     }
-}
-onlineService.init()
+    dksq.init()
+
+
+    try {
+        var loc = "定位中..."
+        var map = new AMap.Map('container', {
+            resizeEnable: true
+        });
+        var options = {
+
+            enableHighAccuracy: true, // 是否使用高精度定位，默认:true
+            timeout: 10000, // 超过10秒后停止定位，默认：无穷大
+            maximumAge: 0, // 定位结果缓存0毫秒，默认：0
+            convert: true, // 自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+        }
+        AMap.plugin(["AMap.Geolocation"], function () {
+            var geolocation = new AMap.Geolocation(options);
+            map.addControl(geolocation);
+            geolocation.getCurrentPosition()
+            AMap.event.addListener(geolocation, 'complete', onComplete); // 返回定位信息
+            AMap.event.addListener(geolocation, 'error', onError); // 返回定位出错信息
+        });
+
+        function onComplete(obj) {
+            loc = obj.addressComponent.city + obj.addressComponent.district + obj.addressComponent.township
+            var msg = JSON.parse(localStorage.getItem("msg"))[0]
+            msg.loc = loc
+            localStorage.setItem("msg", JSON.stringify([msg]))
+            if (locs) {
+                $("#loc").html(loc)
+            }
+        }
+    } catch (error) {
+
+    }
+})
--- view/homePage/insure/js/insureInput.js
+++ view/homePage/insure/js/insureInput.js
@@ -11,38 +11,27 @@ $(function () {
             if (urlIds === "number") {
                 $("#title").html("请输入投保亩数")
                 $("#text").attr("placeholder", "请输入投保亩数")
-                // if(){
-                // $("#text").attr("value", msg.number)
-                // }
-
             } else if (urlIds === "name") {
                 $("#title").html("请输入你的姓名")
                 $("#text").attr("placeholder", "请输入你的姓名")
-                // $("#text").attr("value", msg.name)
             } else if (urlIds === "idType") {
                 $("#title").html("请选择你的证件类型")
                 $("#text").attr("placeholder", "请选择你的证件类型")
-                // $("#text").attr("value", msg.idType)
             } else if (urlIds === "id") {
                 $("#title").html("请输入你的证件号码")
                 $("#text").attr("placeholder", "请输入你的证件号码")
-                // $("#text").attr("value", msg.id)
             } else if (urlIds === "pho") {
                 $("#title").html("请输入你的手机号")
                 $("#text").attr("placeholder", "请输入你的手机号")
-                // $("#text").attr("value", msg.pho)
             } else if (urlIds === "ema") {
                 $("#title").html("请输入你的电子邮箱")
                 $("#text").attr("placeholder", "请输入你的电子邮箱")
-                // $("#text").attr("value", msg.ema)
             } else if (urlIds === "msg") {
                 $("#title").html("请输入承保作物信息")
                 $("#text").attr("placeholder", "请输入承保作物信息")
-                // $("#text").attr("value", msg.msg)
             } else if (urlIds === "ben") {
                 $("#title").html("请输入受益人名称")
                 $("#text").attr("placeholder", "请输入受益人名称")
-                // $("#text").attr("value", msg.ben)
             }
             $(document).on("click", "#sweep", function () {
                 msg[urlIds] = $("#text").val()
@@ -51,24 +40,17 @@ $(function () {
                 } else {
                     localStorage.setItem("msg", JSON.stringify([msg]))
                 }
-
                 window.open("insure.html", "_self");
             })
-            // $(document).on("click", "#historyBack", function () {
-            //     msg[urlIds] = $("#text").val()
-            //     if ($("#text").val() == "") {
-            //         msg[urlIds] = $("#text").attr("placeholder")
-            //     }
-            //     localStorage.setItem("msg", JSON.stringify([msg]))
-            // })
 
             $("input").keyup(function (e) {
                 if (e.keyCode === 13) {
                     msg[urlIds] = $("#text").val()
-                    if ($("#text").val() == "") {
-                        // msg[urlIds] = $("#text").attr("placeholder")
+                    if ($.trim($("#text").val()) == "") {
+                        msg[urlIds] = $("#text").attr("placeholder")
+                    } else {
+                        localStorage.setItem("msg", JSON.stringify([msg]))
                     }
-                    localStorage.setItem("msg", JSON.stringify([msg]))
                     window.open("insure.html", "_self");
                 }
             });
